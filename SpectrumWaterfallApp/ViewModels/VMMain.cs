b707c16cbaba779f8d3fc34b6fbf13d89a4cac21
stb.infoserv <stb.infoserv@gmail.com>
2025-05-29 13:05:30 +0300

    regions added

--- SpectrumWaterfallApp/ViewModels/VMMain.cs
+++ SpectrumWaterfallApp/ViewModels/VMMain.cs
@@ -11,44 +11,23 @@ namespace SpectrumWaterfallApp.ViewModels;
 
 public class VmMain : VmBase
 {
+    #region Properties
+
     public WriteableBitmap SpectrumBitmap { get; }
     public WriteableBitmap WaterfallBitmap { get; }
-
-    private readonly SpectrumDataModel _model;
-    private readonly SpectrumGenerator _generator;
-    private readonly Timer _timer;
-
-    private double _timeOffset = 0;
-    private DateTime? _pauseTime = null;
-
-    private double _zoom = 1.0;
     public double Zoom
     {
         get => _zoom;
         set { _zoom = Math.Clamp(value, 0.1, 10.0); OnPropertyChanged(); }
     }
-
+    #endregion
+    #region Commands
     public ICommand StartCommand { get; }
     public ICommand StopCommand { get; }
     public ICommand ZoomInCommand { get; }
     public ICommand ZoomOutCommand { get; }
-
-    public VmMain()
-    {
-        _model = new SpectrumDataModel(1024, 100, 200);
-        _generator = new SpectrumGenerator(_model.Width);
-
-        SpectrumBitmap = new WriteableBitmap(_model.Width, _model.SpectrumHeight, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null);
-        WaterfallBitmap = new WriteableBitmap(_model.Width, _model.WaterfallHeight, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null);
-
-        StartCommand = new RelayCommand(_ => Start());
-        StopCommand = new RelayCommand(_ => Stop());
-        ZoomInCommand = new RelayCommand(_ => Zoom += 0.1);
-        ZoomOutCommand = new RelayCommand(_ => Zoom -= 0.1);
-
-        _timer = new Timer(50);
-        _timer.Elapsed += Timer_Elapsed;
-    }
+    #endregion
+    #region Private Methods
 
     private void Start()
     {
@@ -75,5 +54,31 @@ public class VmMain : VmBase
             SpectrumRenderer.RenderSpectrumLine(SpectrumBitmap, _model.CurrentSpectrum, _zoom);
             WaterfallRenderer.RenderWaterfall(WaterfallBitmap, _model.CurrentSpectrum, _model.WaterfallPowerMap, _model.WaterfallColorMap, _zoom);
         });
+    }
+    #endregion
+    #region Private Fields
+
+    private readonly SpectrumDataModel _model;
+    private readonly SpectrumGenerator _generator;
+    private readonly Timer _timer;
+    private double _timeOffset = 0;
+    private DateTime? _pauseTime = null;
+    private double _zoom = 1.0;
+    #endregion
+    public VmMain()
+    {
+        _model = new SpectrumDataModel(1024, 100, 200);
+        _generator = new SpectrumGenerator(_model.Width);
+
+        SpectrumBitmap = new WriteableBitmap(_model.Width, _model.SpectrumHeight, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null);
+        WaterfallBitmap = new WriteableBitmap(_model.Width, _model.WaterfallHeight, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null);
+
+        StartCommand = new RelayCommand(_ => Start());
+        StopCommand = new RelayCommand(_ => Stop());
+        ZoomInCommand = new RelayCommand(_ => Zoom += 0.1);
+        ZoomOutCommand = new RelayCommand(_ => Zoom -= 0.1);
+
+        _timer = new Timer(50);
+        _timer.Elapsed += Timer_Elapsed;
     }
 }
